8d1c5f314de7c5857f4f4697791841bbdc8f346b
ElnurKasimov <ekasimov2012@gmail.com>
2024-05-27 21:52:46 +0300

    created description of task A - Chocolate Problem

--- src/main/java/a_ChokolateProblem/Main.java
+++ src/main/java/a_ChokolateProblem/Main.java
@@ -1,6 +1,41 @@
 package a_ChokolateProblem;
 
 public class Main {
+
+//    A
+//
+//    Chocolate problem
+//    Collision of titans, breaking bars, mind games - that's what awaits you in this problem.
+//    There was a game table with a chocolate bar on it.
+//    As always happens in such cases, Buggy and Proggy saw it at the same time and began to pull.
+//    Having not identified the strongest, Buggy took a chance and proposed
+//    The Chocolate Game “All or Nothing”.
+//    The rules of this game are as follows: initially in the hands of the players
+//    there is one piece of chocolate of size NxM.
+//    The first player breaks it into two rectangular parts and leaves them on the game table.
+//    After this, the second player selects one of the pieces on the table and performs
+//    the same operation on it.
+//    A similar procedure is repeated until there are no 1x1 pieces left on the table
+//    (they cannot be broken, chocolate contains strong nuts).
+//    The one who cannot perform the operation loses. All the chocolate.
+//    Proggy decided to go first. Will this help him?
+//
+//    Input format
+//    The only line contains two integer numbers N and M (1 <= N, M <= 70000).
+//
+//    Output format
+//    Output the winner's name: "Proggy" or "Buggy".
+//
+//    Examples
+//    Input
+//      2 2
+//    Output
+//      Proggy
+//    Input
+//      1 3
+//    Output
+//      Buggy
+
     public static void main(String[] args) {
         System.out.println("Hello world!");
     }
